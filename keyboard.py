b56f23243072475dd16570ecf5e3b6c7a87e6211
Artem Flegontov <flegontoff@gmail.com>
2021-10-15 00:12:37 +0300

    resize keyboard

--- keyboard.py
+++ keyboard.py
@@ -3,4 +3,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 #Объяевление кнопок и их название
 button1 = KeyboardButton("анекдот")
-markup1 = ReplyKeyboardMarkup().add(button1)
+markup1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button1)
